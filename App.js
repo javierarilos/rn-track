fa4e7982f46c19d0789ee265de2a88ad930c0881
javierarilos <javier.arias@verse.me>
2020-12-01 07:16:13 +0100

    add empty dummy screens, dummy navigation

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack'
+import AccountScreen from './src/screens/AccountScreen';
+import SigninScreen from './src/screens/SigninScreen';
+import SignupScreen from './src/screens/SignupScreen';
+import TrackCreateScreen from './src/screens/TrackCreateScreen';
+import TrackDetailScreen from './src/screens/TrackDetailScreen';
+import TracksListScreen from './src/screens/TracksListScreen';
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="AccountScreen">
+        <Stack.Screen name="AccountScreen" component={AccountScreen} options={{ title: "AccountScreen" }} />
+        <Stack.Screen name="SigninScreen" component={SigninScreen} options={{ title: "SigninScreen" }} />
+        <Stack.Screen name="SignupScreen" component={SignupScreen} options={{ title: "SignupScreen" }} />
+        <Stack.Screen name="TrackCreateScreen" component={TrackCreateScreen} options={{ title: "TrackCreateScreen" }} />
+        <Stack.Screen name="TrackDetailScreen" component={TrackDetailScreen} options={{ title: "TrackDetailScreen" }} />
+        <Stack.Screen name="TracksListScreen" component={TracksListScreen} options={{ title: "TracksListScreen" }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default () => {
+  return <App />
+};
---
+++ src/screens/TracksListScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const TracksListScreen = () => {
+    return (<View>
+        <Text style={{fontSize: 28}}>TracksListScreen</Text>
+    </View>);
+};
+
+const styles = StyleSheet.create({});
+
+export default TracksListScreen;
